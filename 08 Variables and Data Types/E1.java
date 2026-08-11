34a3cce87723743d9db514d31d7f2392c997eed9
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-08-11 13:06:38 +0530

    Run

---
+++ 08 Variables and Data Types/E1.java
@@ -0,0 +1,22 @@
+//Variables in Java :
+
+/*
+A variables is a named memory location used to store data.
+
+Syntax :
+    dataType variableName = value;
+
+Example :
+    int age = 19;
+    String name = "Shatrudhan";
+*/
+
+public class E1 {
+    public static void main(String args[]) {
+        int age = 19;
+        String name = "Shtrudhan";
+
+        System.out.println(age);
+        System.out.println(name);
+    }
+}
---
+++ 08 Variables and Data Types/F1.java
@@ -0,0 +1,26 @@
+//Data Types in Java :
+
+/*
+There is two types
+
+1. Primitive Data Type :
+    ◇ byte
+    ◇ short
+    ◇ int
+    ◇ long
+    ◇ float
+    ◇ double
+    ◇ char
+    ◇ boolean
+
+2. Non Primitive Data Type :
+    ◇ String
+    ◇ Array
+    ◇ Class
+    ◇ Object
+    ◇ Interface
+*/
+
+public class F1 {
+    
+}
