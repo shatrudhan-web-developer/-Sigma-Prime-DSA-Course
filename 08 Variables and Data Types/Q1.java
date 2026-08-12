e861008310594031f5422f1810e97f6048b6fe59
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-08-12 20:02:43 +0530

    Run

--- 08 Variables and Data Types/Q1.java
+++ 08 Variables and Data Types/Q1.java
@@ -1,6 +1,26 @@
-//How does java code run? :
+// How does java code run? :
 
 /*
 Step 1: Write Source Code :
-    Create a file:
-*/
+                ↓
+        Create a file
+                ↓
+            Hell.java
+
+Step 2. Compilation :
+            ↓
+        javac Hello.java
+
+The java compile converts the source code into Bytecode.
+                        ↓
+                    Hello.class
+
+Step 3. JVM Execution :
+                ↓
+        Run the program
+                ↓
+        java Hello.java
+
+The JVM (Java Virtual Machine) converts the bytecode into machine code and
+executes it.
+ */
