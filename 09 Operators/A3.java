e028e1c45e70d44f7cfe37ac9f0a4c50bcda00c0
Shatrudhan Kumar <shatrudhan.webdev@gmail.com>
2026-08-13 20:27:19 +0530

    Run

---
+++ 09 Operators/A3.java
@@ -0,0 +1,22 @@
+// Types in Operators :
+
+/*
+Operatoers :
+Ans : Operator ek special symbol hota hai jo values/variables par operation
+perform karta hai.
+
+Example :
+    sum = a + b;
+
+Yaha 
+◇ a or b → Operands
+◇ + → Operator
+◇ a + b → Expression
+
+Types :
+1. Arithmetic Operators (Binary/Unary)
+2. relational Operators
+3. Logical Operators
+4. Bitwise Operators
+5. Assignment Operators
+ */
---
+++ 09 Operators/B3.java
@@ -0,0 +1,23 @@
+//Arithmatic Operators :
+
+/*
+Arithmatic operators ka use mathematical calculation ke liye hota hai.
+
+Types :
+1. Binary Operators : (+, -, *, /, %)
+2. Unary Operators : (++, --)
+*/
+
+public class B3 {
+    public static void main(String args[]) {
+
+        int a = 50;
+        int b = 10;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Modulus/Remender = " + (a % b));
+    }
+}
